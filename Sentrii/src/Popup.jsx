aafbe677c140405d27c9ea1970da89d9dfbcb934
Jeff Lee <jefferson0310@gmail.com>
2023-11-04 01:44:06 +0800

    Added Mantine UI & its dependencies

--- Sentrii/src/Popup.jsx
+++ Sentrii/src/Popup.jsx
@@ -1,58 +1,103 @@
 import { useState } from "react";
 import { scanContractQuery } from "./queries/scanContract.js";
+import {
+  Container,
+  ScrollArea,
+  TextInput,
+  Button,
+  Paper,
+  Text,
+} from "@mantine/core";
 
-function Popup() {
+export default function Popup() {
   const [address, setAddress] = useState("");
   const [scanResult, setScanResult] = useState(null);
 
   const handleScan = () => {
-    fetch("https://public-api.de.fi/graphql", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Api-Key": "338a769238d845ce9a421b093082dcfb",
-      },
-      body: JSON.stringify({
-        query: scanContractQuery,
-        variables: { address },
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.errors) {
-          console.error("GraphQL Error:", data.errors);
-        } else {
-          const result = data.data.scannerProject; // Adjusted property name
-          setScanResult(result);
-        }
+    if (address != "") {
+      fetch("https://public-api.de.fi/graphql", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Api-Key": "338a769238d845ce9a421b093082dcfb",
+        },
+        body: JSON.stringify({
+          query: scanContractQuery,
+          variables: { address },
+        }),
       })
-      .catch((error) => {
-        console.error("Request Error:", error);
-      });
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.errors) {
+            console.log("GraphQL Error:", data.errors);
+          } else {
+            const result = data.data.scannerProject;
+            setScanResult(result);
+          }
+        })
+        .catch((error) => {
+          console.log("Request Error:", error);
+        });
+    }
   };
 
   return (
-    <div>
-      <h1>Scan Smart Contract Addresses</h1>
-      <input
-        type="text"
-        placeholder="Enter Contract Address"
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
-      />
-      <button onClick={handleScan}>Scan</button>
+    <Container size="xs">
+      <Paper padding="xs" radius="md">
+        <h1>Scan Smart Contract Addresses</h1>
+        <TextInput
+          placeholder="Enter Contract Address"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+        <Paper padding="sm" radius="sm" style={{ marginTop: "1rem" }}>
+          <Button variant="light" color="blue" onClick={handleScan}>
+            Scan
+          </Button>
+        </Paper>
 
-      {scanResult && (
-        <div>
-          <h2>Scan Result</h2>
-          <p>Name: {scanResult.name}</p>
-          <p>Address: {scanResult.address}</p>
-          <p>Whitelisted: {scanResult.whitelisted ? "Yes" : "No"}</p>
-          <p>Trust Score: {scanResult.stats.percentage}/100</p>
-        </div>
-      )}
-    </div>
+        {scanResult && (
+          <div>
+            <h2>Scan Result</h2>
+            <Paper
+              padding="sm"
+              radius="md"
+              shadow="xs"
+              style={{ marginBottom: "1rem" }}
+            >
+              <Text style={{ fontWeight: "bold" }}>Name:</Text>
+              <Text>{scanResult.name}</Text>
+            </Paper>
+            <Paper
+              padding="sm"
+              radius="md"
+              shadow="xs"
+              style={{
+                marginBottom: "1rem",
+                maxHeight: "150px", // maximum height for card
+              }}
+            >
+              <Text style={{ fontWeight: "bold" }}>Address:</Text>
+              <ScrollArea type="never">
+                <Text>{scanResult.address}</Text>
+              </ScrollArea>
+            </Paper>
+            <Paper
+              padding="sm"
+              radius="md"
+              shadow="xs"
+              style={{ marginBottom: "1rem" }}
+            >
+              <Text style={{ fontWeight: "bold" }}>Whitelisted:</Text>
+              <Text>{scanResult.whitelisted ? "Yes" : "No"}</Text>
+            </Paper>
+            <Paper padding="sm" radius="md" shadow="xs">
+              <Text style={{ fontWeight: "bold" }}>Trust Score:</Text>
+              <Text>{scanResult.stats.percentage}/100</Text>
+            </Paper>
+          </div>
+        )}
+      </Paper>
+    </Container>
   );
 }
-
-export default Popup;
--- Sentrii/src/main.jsx
+++ Sentrii/src/main.jsx
@@ -1,9 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import Popup from "./Popup.jsx";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <MantineProvider>
     <Popup />
-  </React.StrictMode>
+  </MantineProvider>
 );
